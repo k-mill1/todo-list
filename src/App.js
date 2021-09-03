b5b8fa4a0044bbb18111bfe61ce952f24953b2c3
k-mill1 <kaisamill@gmail.com>
2021-09-03 14:42:05 +0100

    make card responsive

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Add from "./Add";
@@ -13,16 +14,16 @@ const App = () => {
   return (
     <>
   <Row>
-    <Col className = "col-3">
+    <Col sm={3}>
     </Col>
-    <Col>
-  <Card className = 'app-card' style={{width: '30rem'}}>
+    <Col >
+  <Card className = 'app-card' style={{maxWidth: '30rem'}}>
     <Card.Body  >
     <Card.Header className = 'todos-header'>My Todo List</Card.Header>
       <Add />
       <Links />
       <VisibleTodoList />
-    <Card.Text style={{textAlign: 'justify'}}>
+    <Card.Text >
     </Card.Text>
     </Card.Body>
   </Card>
